30e303819a17b470b545953d365a0b329e693eca
Sambit haldar <sambithaldar100@gmail.com>
2023-03-04 22:41:59 +0530

    update

--- lib/View/Camerapage.dart
+++ lib/View/Camerapage.dart
@@ -1,8 +1,9 @@
+import 'package:camera_platform_interface/src/types/camera_description.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class CameraPage extends StatefulWidget {
-  const CameraPage({super.key});
+  const CameraPage({super.key, required List<CameraDescription> cameras});
 
   @override
   State<CameraPage> createState() => _CameraPageState();
